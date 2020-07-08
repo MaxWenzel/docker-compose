14819cf46eff3af4059f4e430221197458a5ebf7
Max Wenzel <Max.Wenzel@uniserv.com>
2020-07-08 20:39:21 +0200

    another postgres schema

--- postgres/init/docker_postgres_init.sql
+++ postgres/init/docker_postgres_init.sql
@@ -7,3 +7,13 @@ CREATE DATABASE micronaut
     LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+	
+CREATE USER springboot WITH PASSWORD 'secret' CREATEDB;
+CREATE DATABASE springboot
+    WITH 
+    OWNER = springboot
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;	
